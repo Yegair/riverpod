863a20ba3c9e3181c9f15e607abb314613476258
Remi Rousselet <darky12s@gmail.com>
2026-09-06 14:14:13 +0200

    Apply suggestion from @rrousselGit

--- packages/riverpod/test/src/core/modifiers/select_async_test.dart
+++ packages/riverpod/test/src/core/modifiers/select_async_test.dart
@@ -124,7 +124,7 @@ void main() {
 
         selectedSub.close();
         refresh.complete(2);
-        await container.pump();
+        await container.read(source.future);
 
         expect(sourceSub.read(), const AsyncData(2));
         expect(listenerCalls, 0);
